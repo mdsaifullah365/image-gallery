8962897a1356be73bc4c22d0e2797e0ba05ed39d
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-02 21:58:29 +0600

    add case study modal with rough case study

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,65 +1,82 @@
+import { useState } from "react";
 import { BiLogoGithub, BiLogoLinkedinSquare } from "react-icons/bi";
 import { MdEmail, MdPhone } from "react-icons/md";
+import CaseStudyModal from "./CaseStudyModal";
 
 const Footer = () => {
-  return (
-    <div className="bg-slate-900 text-gray-300 flex flex-col md:flex-row gap-10 justify-between p-8 sm:p-10 md:px-16 md:py-10 rounded-sm shadow-lg">
-      {/* Left Panel */}
-      <div className="flex flex-col items-start gap-2">
-        <p className="uppercase text-xl text-gray-400 font-bold mb-1">
-          Important links
-        </p>
-        {/* LInks */}
-        <button className="hover:underline hover:text-white">
-          Project Description
-        </button>
-        <a
-          href="https://github.com/mdsaifullah365/image-gallery.git"
-          target="_blank"
-          className="hover:underline hover:text-white"
-          rel="noreferrer"
-        >
-          Source Code
-        </a>
-        <a
-          href="https://drive.google.com/file/d/1qKCQbRDGX3EokK0nQKf02ivksedcEFRv/view?usp=sharing"
-          target="_blank"
-          className="hover:underline hover:text-white"
-          rel="noreferrer"
-        >
-          My Resume
-        </a>
-      </div>
+  const [isCaseStudyModalOpen, setIsCaseStudyModalOpen] = useState(false);
 
-      {/* Right Panel */}
-      <div className="flex flex-col items-start md:items-end gap-3">
-        <p className="uppercase text-xl text-gray-400 font-bold mb-1">Social</p>
-        {/* Links */}
-        <div className="flex justify-center gap-5 text-2xl">
-          <a href="tel:+8801766637772">
-            <MdPhone />
-          </a>
-          <a href="mailto:mdsaifullah1302@gmail.com">
-            <MdEmail />
-          </a>
+  return (
+    <>
+      <div className="bg-slate-900 text-gray-300 flex flex-col md:flex-row gap-10 justify-between p-8 sm:p-10 md:px-16 md:py-10 rounded-sm shadow-lg">
+        {/* Left Panel */}
+        <div className="flex flex-col items-start gap-2">
+          <p className="uppercase text-xl text-gray-400 font-bold mb-1">
+            Important links
+          </p>
+          {/* LInks */}
+          <button
+            onClick={() => setIsCaseStudyModalOpen(true)}
+            className="hover:underline hover:text-white"
+          >
+            Case Study
+          </button>
           <a
-            href="https://www.linkedin.com/in/mdsaifullah365/"
+            href="https://github.com/mdsaifullah365/image-gallery.git"
             target="_blank"
+            className="hover:underline hover:text-white"
             rel="noreferrer"
           >
-            <BiLogoLinkedinSquare />
+            Source Code
           </a>
           <a
-            href="https://www.github.com/mdsaifullah365/"
+            href="https://drive.google.com/file/d/1qKCQbRDGX3EokK0nQKf02ivksedcEFRv/view?usp=sharing"
             target="_blank"
+            className="hover:underline hover:text-white"
             rel="noreferrer"
           >
-            <BiLogoGithub />
+            My Resume
           </a>
         </div>
-        {/* <p>&copy; {new Date().getFullYear()}, Md Saif Ullah</p> */}
+
+        {/* Right Panel */}
+        <div className="flex flex-col items-start md:items-end gap-3">
+          <p className="uppercase text-xl text-gray-400 font-bold mb-1">
+            Social
+          </p>
+          {/* Links */}
+          <div className="flex justify-center gap-5 text-2xl">
+            <a href="tel:+8801766637772">
+              <MdPhone />
+            </a>
+            <a href="mailto:mdsaifullah1302@gmail.com">
+              <MdEmail />
+            </a>
+            <a
+              href="https://www.linkedin.com/in/mdsaifullah365/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLogoLinkedinSquare />
+            </a>
+            <a
+              href="https://www.github.com/mdsaifullah365/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <BiLogoGithub />
+            </a>
+          </div>
+          {/* <p>&copy; {new Date().getFullYear()}, Md Saif Ullah</p> */}
+        </div>
       </div>
-    </div>
+
+      {/* Delete Modal */}
+      <CaseStudyModal
+        isCaseStudyModalOpen={isCaseStudyModalOpen}
+        handleClose={() => setIsCaseStudyModalOpen(false)}
+      />
+    </>
   );
 };
 

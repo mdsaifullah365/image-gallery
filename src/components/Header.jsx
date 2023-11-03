cf19656595350b81a3a6fb210c3f6968af37d83f
Md Saif Ullah <mdsaifullah1302@gmail.com>
2023-11-04 05:38:14 +0600

    add case study cta button

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,31 @@
-const Header = () => {
-  return (
-    <div className="bg-white flex justify-between items-center px-5 rounded-sm shadow h-12 md:h-16">
-      {/* Left Panel */}
-      <h1 className="font-bold text-lg md:text-2xl lg:text-3xl">
-        Image Gallery
-      </h1>
+import { useState } from "react";
+import CaseStudyModal from "./CaseStudyModal";
 
-      {/* Right Panel*/}
-      <div></div>
-    </div>
+const Header = () => {
+  const [isCaseStudyModalOpen, setIsCaseStudyModalOpen] = useState(false);
+  return (
+    <>
+      <div className="bg-white flex justify-between items-center px-5 rounded-sm shadow h-12 md:h-16">
+        {/* Logo */}
+        <h1 className="font-bold text-lg md:text-2xl lg:text-3xl">
+          Image Gallery
+        </h1>
+
+        {/* CTA*/}
+        <button
+          onClick={() => setIsCaseStudyModalOpen(true)}
+          className="bg-[#f67d28] text-white text-xs sm:text-base sm:font-semibold px-3 sm:px-6 py-2 sm:py-2 rounded-md cursor-pointer shadow-lg hover:shadow-xl hover:scale-105 transition-transform duration-100 ease-in"
+        >
+          Case Study
+        </button>
+      </div>
+
+      {/* Case Study Modal */}
+      <CaseStudyModal
+        isCaseStudyModalOpen={isCaseStudyModalOpen}
+        handleClose={() => setIsCaseStudyModalOpen(false)}
+      />
+    </>
   );
 };
 
